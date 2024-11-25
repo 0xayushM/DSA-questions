0accdc31c2c697e3a26bf404f427d6a272fbaadf
Ayush Mangal <mangal.ayush.4982@gmail.com>
2024-11-25 22:49:06 +0530

    cses concert tickets

--- CSES/sorting and searching/concert_tickets.cpp
+++ CSES/sorting and searching/concert_tickets.cpp
@@ -1,20 +1,34 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<set>
 using namespace std;
 #define ll long long
 
 int main() {
     ll n,m;
     cin >> n >> m;
-    multiset<ll> prices;
+    multiset<int> tickets;
     for(ll i=0;i<n;i++) {
-        int r;
-        cin >> r;
-        prices.insert(r);
+        int price;
+        cin >> price;
+        tickets.insert(price);
     }
+    vector<int> maxPrices;
     for(ll i=0;i<m;i++) {
-        int r;
-        cin >> r;
-        auto price = prices.upper_bound(r);
-        // if()
+        int price;
+        cin >> price;
+        maxPrices.push_back(price);
     }
+
+    for(int price : maxPrices) {
+        auto it = tickets.upper_bound(price); // finds the first ticket greater than price
+        if(it == tickets.begin()) {
+            cout << -1 << endl;
+        } else {
+            --it;
+            cout << *it << endl;
+            tickets.erase(it);
+        }
+    }
+    return 0;
 }
