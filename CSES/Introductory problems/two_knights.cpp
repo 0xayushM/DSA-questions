8a7c32bedfe0a7ae0074d5161dc3addb01af1334
Ayush Mangal <mangal.ayush.4982@gmail.com>
2024-12-04 21:12:26 +0530

    cses two knights, two sets

--- CSES/Introductory problems/two_knights.cpp
+++ CSES/Introductory problems/two_knights.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 #define ll long long
 
+ll noOfWays(ll n) {
+    ll total = (n*n*((n*n) - 1))/2;
+    ll waysToAttack = 4 * (n-1) * (n-2);
+    return total - waysToAttack;
+}
+
 int main() {
-     
+     ll n;
+     cin >> n;
+     for(ll i = 1; i<=n;i++) {
+        cout << noOfWays(i) << "\n";
+     }
+     return 0;
 }
