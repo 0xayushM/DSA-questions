84524ab6f2dab8df3f02754d4f619bd4f6fafeb8
Ayush Mangal <mangal.ayush.4982@gmail.com>
2025-01-04 08:17:27 +0530

    4th january

---
+++ leetcode/daily questions/january-2025/4_unique_length_3_paindromic_subsequences.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int countPalindromicSubsequence(string s) {
+    return 0;
+}
+
+int main() {
+    string s;
+    cin >> s;
+    cout << countPalindromicSubsequence(s);
+}
